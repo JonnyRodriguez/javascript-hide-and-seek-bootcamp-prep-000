4d0e012c274994044539fbd2568200af101b3b19
Jonny Rodriguez <jojoroto@gmail.com>
2019-03-20 12:35:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+var getFirstSelector = selector => {
+  return document.querySelector(selector);
+};
 
+var nestedTarget = () => {
+  return document.querySelector("#nested .target");
+};
+
+var increaseRankBy = n => {
+  let list=document.querySelectorAll(".ranked-list");
+  for( let l=0; l<list.length; l++) {
+    list[l].innerHTML = parseInt(list[l].innerHTML)+n;
+  }
+};
+
+var deepestChild = () => {
+  let node = document.querySelector("div#grand-node");
+  while( node.firstElementChild ) {
+    node=node.firstElementChild;
+  }
+  return node;
+};
